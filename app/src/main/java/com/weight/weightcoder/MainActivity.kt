ecdc209193fb89db0babdb6303d29ce19d25f499
flibielt <hcsabi98@gmail.com>
2019-08-20 15:31:02 +0200

    Created the main activity layout

--- app/src/main/java/com/weight/weightcoder/MainActivity.kt
+++ app/src/main/java/com/weight/weightcoder/MainActivity.kt
@@ -2,6 +2,7 @@ package com.weight.weightcoder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.TextView
 import com.weight.weightcoder.coder.WeightCode
 import kotlinx.android.synthetic.*
@@ -14,6 +15,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var textView = findViewById<TextView>(R.id.result_text_view)
+        var editText = findViewById<EditText>(R.id.input_text)
+        editText.hint = "The input text"
         textView.text = WeightCode.code("ASD")
 
     }
